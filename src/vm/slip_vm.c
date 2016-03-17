7c60546d1332c1a0c05fda064e76aed9baccb7c5
Willem Victor <wimpievictor@gmail.com>
2016-03-17 11:57:32 +0200

    Initialize VM

--- src/vm/slip_vm.c
+++ src/vm/slip_vm.c
@@ -9,8 +9,41 @@ void slipInitConfig(SlipConfig* config) {
 SlipVM* slipNewVM(SlipConfig* config) {
     SlipVM* vm = (SlipVM*)malloc(sizeof(SlipVM));
 
+    // Memory
     vm->memory = (SlipByte*)malloc(SLIP_MEM * sizeof(SlipByte));
+
+    for (int i = 0; i < SLIP_MEM; i++) {
+        vm->memory = 0x0;
+    }
+
+    // Registers
+    for (int i = 0; i < SLIP_REGISTERS; i++) {
+        vm->V[i] = 0x0;
+    }
+
+    vm->I = 0x0;
+
+    // Stack
+    vm->SP = 0x0;
+
+    for (int i = 0; i < SLIP_STACK; i++) {
+        vm->stack[i] = 0x0;
+    }
+
+    // Timers
+    vm->delayTimer = 0x0;
+    vm->soundTimer = 0x0;
+
+    // Display
     vm->display = (SlipByte*)malloc(SLIP_SCREEN_WIDTH * SLIP_SCREEN_HEIGHT * sizeof(SlipByte));
+
+    // Keys
+    for (int i = 0; i < SLIP_KEYS; i++) {
+        vm->keys[i] = 0x0;
+    }
+
+    // Program Counter
+    vm->PC = 0x0;
 
     return vm;
 }
